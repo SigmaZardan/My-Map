2ed7c50da5eabd8beb6f787ecd4fe4eae67b5f69
Bibek Bhujel <biekbhujel077@gmail.com>
2023-03-20 15:48:09 +0545

    First commit after setting up the environment

---
+++ app/src/main/java/com/example/mymaps/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mymaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // set the layout manager on the recycler view
+
+        // set the adapter on the recycler view
+
+    }
+}
